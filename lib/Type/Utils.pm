540028287e65e63e820c96608d5935a36d2b1197
Toby Inkster <mail@tobyinkster.co.uk>
2013-04-01 22:54:51 +0100

    docs for Type::Utils

--- lib/Type/Utils.pm
+++ lib/Type/Utils.pm
@@ -24,6 +24,7 @@ our @EXPORT_OK = (@EXPORT, qw< type subtype >);
 
 sub extends
 {
+	_confess "not a type library" unless caller->isa("Type::Library");
 	my $caller = caller->meta;
 	
 	foreach my $lib (@_)
@@ -221,3 +222,139 @@ sub via (&;@)
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+Type::Utils - utility functions to make defining type constraints a little easier
+
+=head1 SYNOPSIS
+
+   package MyTypes;
+   
+   use base "Type::Library";
+   use Type::Utils;
+   
+   extends "Type::Standard";
+   
+   declare "AllCaps",
+      as "Str",
+      where { uc($_) eq $_ },
+      inline_as { my $varname = $_[1]; "uc($varname) eq $varname" };
+   
+   coerce "AllCaps",
+      from "Str", via { uc($_) };
+
+=head1 DESCRIPTION
+
+This module provides utility functions to make defining type constraints a
+little easier. By default, all of the functions documented below are
+exported, except C<subtype> and C<type> (use C<declare> instead).
+
+=head2 Moose::Util::TypeConstraints-like
+
+The following are similar to the similarly named functions described in
+L<Moose::Util::TypeConstraints>.
+
+=over
+
+=item C<< subtype $name, %options >>
+
+=item C<< subtype %options >>
+
+=item C<< type $name, %options >>
+
+=item C<< type %options >>
+
+=item C<< as $parent >>
+
+=item C<< where { BLOCK } >>
+
+=item C<< message { BLOCK } >>
+
+=item C<< inline_as { BLOCK } >>
+
+=item C<< class_type $name, { class => $package, %options } >>
+
+=item C<< class_type { class => $package, %options } >>
+
+=item C<< role_type $name, { role => $package, %options } >>
+
+=item C<< role_type { role => $package, %options } >>
+
+=item C<< duck_type $name, \@methods >>
+
+=item C<< duck_type \@methods >>
+
+=item C<< union $name, \@constraints >>
+
+=item C<< union \@constraints >>
+
+=item C<< enum $name, \@values >>
+
+=item C<< enum \@values >>
+
+=item C<< coerce $target, @coercions >>
+
+=item C<< from $source >>
+
+=item C<< via { BLOCK } >>
+
+=back
+
+=head2 Other
+
+=over
+
+=item C<< declare $name, %options >>
+
+=item C<< declare %options >>
+
+C<declare> is a function which works like C<subtype> and C<type>. In fact,
+the latter pair are just aliases for the former.
+
+=item C<< intersection $name, \@constraints >>
+
+=item C<< intersection \@constraints >>
+
+Defines a type constraint which is the intersection of several existing
+constraints.
+
+=item C<< extends @library >>
+
+Indicates that this type library extends other type libraries, importing
+their type constraints.
+
+=back
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Type-Tiny>.
+
+=head1 SEE ALSO
+
+L<Type::Tiny>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
