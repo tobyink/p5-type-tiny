557e37f2c112cb5b93bd3791b73657e82200e962
Toby Inkster <mail@tobyinkster.co.uk>
2013-04-02 14:35:29 +0100

    ws

--- examples/datetimes-from-manual.pl
+++ examples/datetimes-from-manual.pl
@@ -2,38 +2,38 @@ use strict;
 use warnings;
 
 BEGIN {
-   package MyTypes::DateTime;
-   
-   use Type::Library
-      -base,
-      -declare => qw( Datetime DatetimeHash EpochHash );
-   use Type::Utils;
-   use Type::Standard -types;
-   
-   class_type Datetime, { class => "DateTime" };
-   
-   declare DatetimeHash,
-      as Dict[
-         year       => Int,
-         month      => Optional[ Int ],
-         day        => Optional[ Int ],
-         hour       => Optional[ Int ],
-         minute     => Optional[ Int ],
-         second     => Optional[ Int ],
-         nanosecond => Optional[ Int ],
-         time_zone  => Optional[ Str ],
-      ];
-   
-   declare EpochHash,
-      as Dict[ epoch => Int ];
-   
-   coerce Datetime,
-      from Int,          via { "DateTime"->from_epoch(epoch => $_) },
-      from Undef,        via { "DateTime"->now },
-      from DatetimeHash, via { "DateTime"->new(%$_) },
-      from EpochHash,    via { "DateTime"->from_epoch(%$_) };
-   
-   1;
+	package MyTypes::DateTime;
+	
+	use Type::Library
+		-base,
+		-declare => qw( Datetime DatetimeHash EpochHash );
+	use Type::Utils;
+	use Type::Standard -types;
+	
+	class_type Datetime, { class => "DateTime" };
+	
+	declare DatetimeHash,
+		as Dict[
+			year       => Int,
+			month      => Optional[ Int ],
+			day        => Optional[ Int ],
+			hour       => Optional[ Int ],
+			minute     => Optional[ Int ],
+			second     => Optional[ Int ],
+			nanosecond => Optional[ Int ],
+			time_zone  => Optional[ Str ],
+		];
+	
+	declare EpochHash,
+		as Dict[ epoch => Int ];
+	
+	coerce Datetime,
+		from Int,          via { "DateTime"->from_epoch(epoch => $_) },
+		from Undef,        via { "DateTime"->now },
+		from DatetimeHash, via { "DateTime"->new(%$_) },
+		from EpochHash,    via { "DateTime"->from_epoch(%$_) };
+	
+	1;
 };
 
 no thanks "MyTypes::DateTime";
