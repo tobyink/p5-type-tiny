eab58b2cf33747b7672e822a283521d1a9c09797
Toby Inkster <mail@tobyinkster.co.uk>
2013-04-10 20:39:05 +0100

    build inline coercions within thunks - optimization
    --HG--
    branch : feature/auto-coercions

--- lib/Types/Standard/AutomaticCoercion.pm
+++ lib/Types/Standard/AutomaticCoercion.pm
@@ -10,6 +10,14 @@ BEGIN {
 
 require Type::Coercion;
 
+sub Stringable (&)
+{
+	package #private
+	Types::Standard::AutomaticCoercion::_Stringable;
+	use overload q[""] => sub { $_[0]{text} ||= $_[0]{code}->() }, fallback => 1;
+	bless +{ code => $_[0] };
+}
+
 my $lib = "Types::Standard"->meta;
 
 $lib->get_type("ArrayRef")->{coercion_generator} = sub
@@ -22,15 +30,17 @@ $lib->get_type("ArrayRef")->{coercion_generator} = sub
 	
 	if ($param->coercion->can_be_inlined)
 	{
-		my @code;
-		push @code, 'do { my ($orig, $return_orig, @new) = ($_, 0);';
-		push @code,    'for (@$orig) {';
-		push @code, sprintf('$return_orig++ && last unless (%s);', $coercable_item->inline_check('$_'));
-		push @code, sprintf('push @new, (%s);', $param->coercion->inline_coercion('$_'));
-		push @code,    '}';
-		push @code,    '$return_orig ? $orig : \\@new';
-		push @code, '}';
-		$C->add_type_coercions($parent => "@code");
+		$C->add_type_coercions($parent => Stringable {
+			my @code;
+			push @code, 'do { my ($orig, $return_orig, @new) = ($_, 0);';
+			push @code,    'for (@$orig) {';
+			push @code, sprintf('$return_orig++ && last unless (%s);', $coercable_item->inline_check('$_'));
+			push @code, sprintf('push @new, (%s);', $param->coercion->inline_coercion('$_'));
+			push @code,    '}';
+			push @code,    '$return_orig ? $orig : \\@new';
+			push @code, '}';
+			"@code";
+		});
 	}
 	else
 	{
@@ -61,15 +71,17 @@ $lib->get_type("HashRef")->{coercion_generator} = sub
 	
 	if ($param->coercion->can_be_inlined)
 	{
-		my @code;
-		push @code, 'do { my ($orig, $return_orig, %new) = ($_, 0);';
-		push @code,    'for (keys %$orig) {';
-		push @code, sprintf('$return_orig++ && last unless (%s);', $coercable_item->inline_check('$orig->{$_}'));
-		push @code, sprintf('$new{$_} = (%s);', $param->coercion->inline_coercion('$orig->{$_}'));
-		push @code,    '}';
-		push @code,    '$return_orig ? $orig : \\%new';
-		push @code, '}';
-		$C->add_type_coercions($parent => "@code");
+		$C->add_type_coercions($parent => Stringable {
+			my @code;
+			push @code, 'do { my ($orig, $return_orig, %new) = ($_, 0);';
+			push @code,    'for (keys %$orig) {';
+			push @code, sprintf('$return_orig++ && last unless (%s);', $coercable_item->inline_check('$orig->{$_}'));
+			push @code, sprintf('$new{$_} = (%s);', $param->coercion->inline_coercion('$orig->{$_}'));
+			push @code,    '}';
+			push @code,    '$return_orig ? $orig : \\%new';
+			push @code, '}';
+			"@code";
+		});
 	}
 	else
 	{
@@ -100,15 +112,17 @@ $lib->get_type("ScalarRef")->{coercion_generator} = sub
 	
 	if ($param->coercion->can_be_inlined)
 	{
-		my @code;
-		push @code, 'do { my ($orig, $return_orig, $new) = ($_, 0);';
-		push @code,    'for ($$orig) {';
-		push @code, sprintf('$return_orig++ && last unless (%s);', $coercable_item->inline_check('$_'));
-		push @code, sprintf('$new = (%s);', $param->coercion->inline_coercion('$_'));
-		push @code,    '}';
-		push @code,    '$return_orig ? $orig : \\$new';
-		push @code, '}';
-		$C->add_type_coercions($parent => "@code");
+		$C->add_type_coercions($parent => Stringable {
+			my @code;
+			push @code, 'do { my ($orig, $return_orig, $new) = ($_, 0);';
+			push @code,    'for ($$orig) {';
+			push @code, sprintf('$return_orig++ && last unless (%s);', $coercable_item->inline_check('$_'));
+			push @code, sprintf('$new = (%s);', $param->coercion->inline_coercion('$_'));
+			push @code,    '}';
+			push @code,    '$return_orig ? $orig : \\$new';
+			push @code, '}';
+			"@code";
+		});
 	}
 	else
 	{
@@ -141,19 +155,21 @@ $lib->get_type("Map")->{coercion_generator} = sub
 	if ((!$kparam->has_coercion or $kparam->coercion->can_be_inlined)
 	and (!$vparam->has_coercion or $vparam->coercion->can_be_inlined))
 	{
-		my @code;
-		push @code, 'do { my ($orig, $return_orig, %new) = ($_, 0);';
-		push @code,    'for (keys %$orig) {';
-		push @code, sprintf('$return_orig++ && last unless (%s);', $kcoercable_item->inline_check('$_'));
-		push @code, sprintf('$return_orig++ && last unless (%s);', $vcoercable_item->inline_check('$orig->{$_}'));
-		push @code, sprintf('$new{(%s)} = (%s);',
-			$kparam->has_coercion ? $kparam->coercion->inline_coercion('$_') : '$_',
-			$vparam->has_coercion ? $vparam->coercion->inline_coercion('$orig->{$_}') : '$orig->{$_}',
-		);
-		push @code,    '}';
-		push @code,    '$return_orig ? $orig : \\%new';
-		push @code, '}';
-		$C->add_type_coercions($parent => "@code");
+		$C->add_type_coercions($parent => Stringable {
+			my @code;
+			push @code, 'do { my ($orig, $return_orig, %new) = ($_, 0);';
+			push @code,    'for (keys %$orig) {';
+			push @code, sprintf('$return_orig++ && last unless (%s);', $kcoercable_item->inline_check('$_'));
+			push @code, sprintf('$return_orig++ && last unless (%s);', $vcoercable_item->inline_check('$orig->{$_}'));
+			push @code, sprintf('$new{(%s)} = (%s);',
+				$kparam->has_coercion ? $kparam->coercion->inline_coercion('$_') : '$_',
+				$vparam->has_coercion ? $vparam->coercion->inline_coercion('$orig->{$_}') : '$orig->{$_}',
+			);
+			push @code,    '}';
+			push @code,    '$return_orig ? $orig : \\%new';
+			push @code, '}';
+			"@code";
+		});
 	}
 	else
 	{
@@ -175,6 +191,8 @@ $lib->get_type("Map")->{coercion_generator} = sub
 	return $C;
 };
 
+# XXX - also Maybe[`a]?
+# XXX - does not seem quite right
 $lib->get_type("Optional")->{coercion_generator} = sub
 {
 	my ($parent, $child, $param) = @_;
@@ -197,49 +215,50 @@ $lib->get_type("Dict")->{coercion_generator} = sub
 
 	if ($all_inlinable)
 	{
-		require B;
-		
-		my $label = sprintf("LABEL%d", ++$label_counter);
-		my @code;
-		push @code, 'do { my ($orig, $return_orig, %tmp, %new) = ($_, 0);';
-		push @code,       "$label: {";
-		for my $k (keys %dict)
-		{
-			my $ct = $dict{$k};
-			my $ct_coerce   = $ct->has_coercion;
-			my $ct_optional = $ct->is_a_type_of(Types::Standard::Optional());
-			my $K = B::perlstring($k);
+		$C->add_type_coercions($parent => Stringable {
+			require B;
 			
-			if ($ct_coerce)
+			my $label = sprintf("LABEL%d", ++$label_counter);
+			my @code;
+			push @code, 'do { my ($orig, $return_orig, %tmp, %new) = ($_, 0);';
+			push @code,       "$label: {";
+			for my $k (keys %dict)
 			{
-				push @code, sprintf('%%tmp = (); $tmp{x} = %s;', $ct->coercion->inline_coercion("\$orig->{$K}"));
-				push @code, sprintf(
-					$ct_optional
-						? 'if (%s) { $new{%s}=$tmp{x} }'
-						: 'if (%s) { $new{%s}=$tmp{x} } else { $return_orig = 1; last %s }',
-					$ct->inline_check('$tmp{x}'),
-					$K,
-					$label,
-				);
+				my $ct = $dict{$k};
+				my $ct_coerce   = $ct->has_coercion;
+				my $ct_optional = $ct->is_a_type_of(Types::Standard::Optional());
+				my $K = B::perlstring($k);
+				
+				if ($ct_coerce)
+				{
+					push @code, sprintf('%%tmp = (); $tmp{x} = %s;', $ct->coercion->inline_coercion("\$orig->{$K}"));
+					push @code, sprintf(
+						$ct_optional
+							? 'if (%s) { $new{%s}=$tmp{x} }'
+							: 'if (%s) { $new{%s}=$tmp{x} } else { $return_orig = 1; last %s }',
+						$ct->inline_check('$tmp{x}'),
+						$K,
+						$label,
+					);
+				}
+				else
+				{
+					push @code, sprintf(
+						$ct_optional
+							? 'if (%s) { $new{%s}=$orig->{%s} }'
+							: 'if (%s) { $new{%s}=$orig->{%s} } else { $return_orig = 1; last %s }',
+						$ct->inline_check("\$orig->{$K}"),
+						$K,
+						$K,
+						$label,
+					);
+				}
 			}
-			else
-			{
-				push @code, sprintf(
-					$ct_optional
-						? 'if (%s) { $new{%s}=$orig->{%s} }'
-						: 'if (%s) { $new{%s}=$orig->{%s} } else { $return_orig = 1; last %s }',
-					$ct->inline_check("\$orig->{$K}"),
-					$K,
-					$K,
-					$label,
-				);
-			}
-		}
-		push @code,       '}';
-		push @code,    '$return_orig ? $orig : \\%new';
-		push @code, '}';
-		#warn "CODE: @code";
-		$C->add_type_coercions($parent => "@code");
+			push @code,       '}';
+			push @code,    '$return_orig ? $orig : \\%new';
+			push @code, '}';
+			"@code";
+		});
 	}
 	
 	else
@@ -304,59 +323,61 @@ $lib->get_type("Tuple")->{coercion_generator} = sub
 
 	if ($all_inlinable)
 	{
-		my $label = sprintf("LABEL%d", ++$label_counter);
-		my @code;
-		push @code, 'do { my ($orig, $return_orig, @tmp, @new) = ($_, 0);';
-		push @code,       "$label: {";
-		for my $i (0 .. $#tuple)
-		{
-			my $ct = $tuple[$i];
-			my $ct_coerce   = $ct->has_coercion;
-			my $ct_optional = $ct->is_a_type_of(Types::Standard::Optional());
-			
-			if ($ct_coerce)
+		$C->add_type_coercions($parent => Stringable {
+			my $label = sprintf("LABEL%d", ++$label_counter);
+			my @code;
+			push @code, 'do { my ($orig, $return_orig, @tmp, @new) = ($_, 0);';
+			push @code,       "$label: {";
+			for my $i (0 .. $#tuple)
 			{
-				push @code, sprintf('@tmp = (); $tmp[0] = %s;', $ct->coercion->inline_coercion("\$orig->[$i]"));
-				push @code, sprintf(
-					$ct_optional
-						? 'if (%s) { $new[%d]=$tmp[0] }'
-						: 'if (%s) { $new[%d]=$tmp[0] } else { $return_orig = 1; last %s }',
-					$ct->inline_check('$tmp[0]'),
-					$i,
-					$label,
-				);
+				my $ct = $tuple[$i];
+				my $ct_coerce   = $ct->has_coercion;
+				my $ct_optional = $ct->is_a_type_of(Types::Standard::Optional());
+				
+				if ($ct_coerce)
+				{
+					push @code, sprintf('@tmp = (); $tmp[0] = %s;', $ct->coercion->inline_coercion("\$orig->[$i]"));
+					push @code, sprintf(
+						$ct_optional
+							? 'if (%s) { $new[%d]=$tmp[0] }'
+							: 'if (%s) { $new[%d]=$tmp[0] } else { $return_orig = 1; last %s }',
+						$ct->inline_check('$tmp[0]'),
+						$i,
+						$label,
+					);
+				}
+				else
+				{
+					push @code, sprintf(
+						$ct_optional
+							? 'if (%s) { $new[%d]=$orig->[%s] }'
+							: 'if (%s) { $new[%d]=$orig->[%s] } else { $return_orig = 1; last %s }',
+						$ct->inline_check("\$orig->[$i]"),
+						$i,
+						$i,
+						$label,
+					);
+				}
 			}
-			else
+			if ($slurpy)
 			{
+				my $size = @tuple;
+				push @code, sprintf('if (@$orig > %d) {', $size);
+				push @code, sprintf('my $tail = [ @{$orig}[%d .. $#$orig] ];', $size);
+				push @code, $slurpy->has_coercion
+					? sprintf('$tail = %s;', $slurpy->coercion->inline_coercion('$tail'))
+					: '';
 				push @code, sprintf(
-					$ct_optional
-						? 'if (%s) { $new[%d]=$orig->[%s] }'
-						: 'if (%s) { $new[%d]=$orig->[%s] } else { $return_orig = 1; last %s }',
-					$ct->inline_check("\$orig->[$i]"),
-					$i,
-					$i,
-					$label,
+					'(%s) ? push(@new, @$tail) : ($return_orig++);',
+					$slurpy->inline_check('$tail'),
 				);
+				push @code, '}';
 			}
-		}
-		if ($slurpy)
-		{
-			my $size = @tuple;
-			push @code, sprintf('if (@$orig > %d) {', $size);
-			push @code, sprintf('my $tail = [ @{$orig}[%d .. $#$orig] ];', $size);
-			push @code, $slurpy->has_coercion
-				? sprintf('$tail = %s;', $slurpy->coercion->inline_coercion('$tail'))
-				: '';
-			push @code, sprintf(
-				'(%s) ? push(@new, @$tail) : ($return_orig++);',
-				$slurpy->inline_check('$tail'),
-			);
+			push @code,       '}';
+			push @code,    '$return_orig ? $orig : \\@new';
 			push @code, '}';
-		}
-		push @code,       '}';
-		push @code,    '$return_orig ? $orig : \\@new';
-		push @code, '}';
-		$C->add_type_coercions($parent => "@code");
+			"@code";
+		});
 	}
 	
 	else
