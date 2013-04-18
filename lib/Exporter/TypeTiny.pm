890b6299c12c6c03b84d483fa0ca042281876dcf
Toby Inkster <mail@tobyinkster.co.uk>
2013-04-18 10:24:35 +0100

    Write Exporter::TypeTiny - an exporter wil similar configuration style to Exporter.pm but a closer feature set to Sub::Exporter
    --HG--
    extra : source : 2d6000ec37a4d1a79254dd1f1b3e5f9e7cd6d350

--- lib/Exporter/TypeTiny.pm
+++ lib/Exporter/TypeTiny.pm
@@ -1,5 +1,115 @@
 package Exporter::TypeTiny;
 
+use 5.008001;
+use strict;   no strict qw(refs);
+use warnings; no warnings qw(void once uninitialized numeric redefine);
+
+sub _croak ($;@) {
+	require Carp;
+	@_ = sprintf($_[0], @_[1..$#_]) if @_ > 1;
+	goto \&Carp::croak;
+}
+
+sub import
+{
+	my $class = shift;
+	my @args  = @_ ? @_ : @{"$class\::EXPORT"};
+	my $opts  = mkopt(\@args);
+
+	my $global_opts = { -into => scalar caller };
+	my @want;
+	
+	while (@$opts)
+	{
+		my $opt = shift @{$opts};
+		my ($name, $value) = @$opt;
+		
+		$name =~ /^[:-](.+)$/
+			? push(@$opts, $class->_exporter_expand_tag($1, $value, $global_opts))
+			: push(@want, $opt);
+	}
+	
+	$class->_exporter_validate_opts($global_opts);
+	
+	for my $wanted (@want)
+	{
+		my %symbols = $class->_exporter_expand_sub(@$wanted, $global_opts);
+		for my $name (keys %symbols)
+		{
+			$class->_exporter_install_sub($name, $wanted->[1], $global_opts, $symbols{$name});
+		}
+	}
+}
+
+sub _exporter_validate_opts
+{
+	1;
+}
+
+sub _exporter_expand_tag
+{
+	my $class = shift;
+	my ($name, $value, $globals) = @_;
+	my $tags  = \%{"$class\::EXPORT_TAGS"};
+	
+	if (exists $tags->{$name})
+	{
+		return map(($_=>$value), @{$tags->{$name}});
+	}
+	elsif ($name eq 'all')
+	{
+		return map(($_=>$value), @{"$class\::EXPORT"}, @{"$class\::EXPORT_OK"});
+	}
+	elsif ($name eq 'default')
+	{
+		return map(($_=>$value), @{"$class\::EXPORT"});
+	}
+	else
+	{
+		$globals->{$name} = $value || 1;
+		return;
+	}
+}
+
+sub _exporter_expand_sub
+{
+	my $class = shift;
+	my ($name, $value, $globals) = @_;
+	
+	if (exists &{"$class\::$name"})
+	{
+		return($name => \&{"$class\::$name"});
+	}
+	
+	_croak("Could not find sub '$name' to export in package '$class'");
+}
+
+sub _exporter_install_sub
+{
+	my $class = shift;
+	my ($name, $value, $globals, $sym) = @_;
+	
+	$name = $value->{-as} || $name;
+	
+	if (ref($name) eq q(SCALAR))
+	{
+		$$name = $sym;
+		return;
+	}
+	
+	if (my $prefix = $value->{-prefix} || $globals->{-prefix})
+	{
+		$name = "$prefix$name";
+	}
+	if (my $suffix = $value->{-suffix} || $globals->{-suffix})
+	{
+		$name = "$name$suffix";
+	}
+	
+	my $into = $globals->{-into};
+	*{"$into\::$name"} = $sym;
+}
+
 sub mkopt
 {
 	my ($in) = @_;
@@ -23,6 +133,8 @@ sub mkopt
 	
 	return \@out;
 }
+
+1;
 
 __END__
 
