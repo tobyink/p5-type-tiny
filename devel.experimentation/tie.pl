4175209a2dc388a42a2cfcf0074ea082b035706e
Toby Inkster <mail@tobyinkster.co.uk>
2013-04-28 09:40:30 +0100

    more Type::Tie experiments

--- devel.experimentation/tie.pl
+++ devel.experimentation/tie.pl
@@ -1,38 +1,42 @@
-use v5.14;
+use strict;
+use warnings;
 
 BEGIN {
-	package Type::Tie; no thanks;
+	package Type::Tie;
+	$INC{"Type/Tie.pm"} = __FILE__;
 	
 	use Carp ();
 	use Role::Tiny ();
 	
 	use base "Exporter::TypeTiny";
-	our @EXPORT = qw(constrain);
+	our @EXPORT = qw(ttie);
 
-	sub constrain (\[$@%]$)
+	sub ttie (\[$@%]$;@)#>&%*/&<%\$[]^!@;@)
 	{
-		my ($ref, $type) = @_;
+		my ($ref, $type, @vals) = @_;
 		
 		if (ref($ref) eq "HASH")
 		{
 			tie(%$ref, "Type::Tie::HASH", $type);
-			return %$ref;
+			%$ref = @vals if @vals;
 		}
 		elsif (ref($ref) eq "ARRAY")
 		{
 			tie(@$ref, "Type::Tie::ARRAY", $type);
-			return @$ref;
+			@$ref = @vals if @vals;
 		}
 		else
 		{
 			tie($$ref, "Type::Tie::SCALAR", $type);
-			return $$ref;
+			$$ref = $vals[0] if @vals;
 		}
+		return $ref;
 	}
 	
 	sub _make_role
 	{
 		my ($me, $type) = @_;
+		
 		my $rolename = sprintf("%s::Constraint%d", $me, $type->{uniq});
 		return $rolename if $rolename->can('assert_valid');
 		
@@ -138,14 +142,33 @@ BEGIN {
 {
 	package Foo;
 	
+	use Data::Dumper;
 	use Types::Standard -types;
+	use Type::Utils;
 	use Type::Tie;
 	
-	constrain(my $v, Int);
-	$v = 1;
-	say $v;
+	ttie my $v, Int, 0;
+	$v++;
+	print $v, $/;
 
-	constrain(my @V, Int);
-	push @V, 1, 2, 3.3;
-	say "@V";
+	ttie my @V, Int;
+	push @V, 1, 2, 3;
+	print "@V", $/;
+	
+	# Here's a weakness
+	ttie my $o, class_type { class => "Foo" };
+	$o = bless [], "Foo";
+	bless $o, "Bar"; # does not call STORE on tied object!
+	print $o, $/;
+	
+	# Here's another weakness
+	ttie my $a, (ArrayRef[Int]), [1, 2, 3];
+	print Dumper($a);
+	push @$a, 4.4; # does not call STORE on tied object!
+	
+	# Here's a workaround
+	ttie my @b, Int, 1, 2, 3;
+	my $b = \@b;   # Here's a ref to a tied array instead.
+	print Dumper($b);
+	push @$b, 4.4; # does call STORE!
 }
