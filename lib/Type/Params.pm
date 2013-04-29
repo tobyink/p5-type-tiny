4548a0453e86fff16ef1f317bba0118e824854d8
Toby Inkster <mail@tobyinkster.co.uk>
2013-04-29 17:42:46 +0100

    fix warnings??

--- lib/Type/Params.pm
+++ lib/Type/Params.pm
@@ -130,7 +130,7 @@ sub compile
 	
 	push @code, '@R;';
 	
-	my $comment = sprintf('line 0 "parameter validation for %s"', [caller 1]->[3]);
+	my $comment = sprintf('line 0 "parameter validation for %s"', [caller 1]->[3] || '__ANON__');
 	my $source  = "# $comment\nsub { no warnings; ".join("\n", @code)." };";
 	
 	return eval_closure(
