8602fac9103349140d0e86c455a123c72f087240
Toby Inkster <mail@tobyinkster.co.uk>
2013-05-10 08:06:18 +0100

    Add ->explain method to Type::Exception::Assertion

--- lib/Type/Exception.pm
+++ lib/Type/Exception.pm
@@ -41,4 +41,56 @@ BEGIN {
 sub type       { $_[0]{type} };
 sub value      { $_[0]{value} };
 
+sub has_type   { defined $_[0]{type} }; # sic
+
+sub explain
+{
+	my $e = shift;
+	return [] unless $e->has_type;
+	$e->_explain($e->type);
+}
+
+sub _explain
+{
+	my $e = shift;
+	my ($type, $value) = @_;
+	$value = $e->value if @_ < 2;
+	
+	return if $type->check($value);
+	
+	if ($type->has_parent)
+	{
+		my $parent = $e->_explain($type->parent);
+		return [
+			sprintf('%s is a subtype of %s', $type, $type->parent),
+			@$parent,
+		] if $parent;
+	}
+	
+	return [
+		sprintf('%s fails type constraint %s', Type::Tiny::_dd($value), $type),
+		sprintf('%s is defined as: %s', $type, $e->_codefor($type)),
+	];
+}
+
+my $b;
+sub _codefor
+{
+	shift;
+	my $type = $_[0];
+	
+	return $type->inline_check('$_')
+		if $type->can_be_inlined;
+	
+	$b ||= do {
+		require B::Deparse;
+		my $tmp = "B::Deparse"->new;
+		$tmp->ambient_pragmas(strict => "all", warnings => "all");
+		$tmp;
+	};
+	my $code = $b->coderef2text($type->constraint);
+	$code =~ s/\s+/ /g;
+	return "sub $code";
+}
+
 1;
