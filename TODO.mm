d6685e9be929ecb3652087001fb6001f3ea4361a
Toby Inkster <mail@tobyinkster.co.uk>
2013-05-15 22:34:28 +0100

    update todo

--- TODO.mm
+++ TODO.mm
@@ -2,8 +2,12 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1365159230192" ID="ID_28734446" MODIFIED="1367787864051" TEXT="Type-Tiny">
 <node CREATED="1366904774462" ID="ID_1574254872" MODIFIED="1366904780973" POSITION="left" TEXT="improved error messages">
-<node CREATED="1366904783424" ID="ID_854471961" MODIFIED="1366904804409" TEXT="an ArrayRef[Int] should be able to tell you what elements are not Ints"/>
-<node CREATED="1366904810438" ID="ID_1602844550" MODIFIED="1366904850449" TEXT="if something is not a number, then the Int error message should report that; not just report that it&apos;s not an Int"/>
+<node CREATED="1368653400604" ID="ID_1010731489" MODIFIED="1368653404711" TEXT="Tuple"/>
+<node CREATED="1368653406025" ID="ID_296421665" MODIFIED="1368653408733" TEXT="Dict"/>
+<node CREATED="1368653409202" ID="ID_490679779" MODIFIED="1368653413290" TEXT="Optional"/>
+<node CREATED="1368653413988" ID="ID_473827694" MODIFIED="1368653420550" TEXT="Maybe"/>
+<node CREATED="1368653426138" ID="ID_864109055" MODIFIED="1368653432167" TEXT="StrMatch??"/>
+<node CREATED="1368653433078" ID="ID_824114712" MODIFIED="1368653435724" TEXT="Ref??"/>
 </node>
 <node CREATED="1367787794514" ID="ID_1248980001" MODIFIED="1367787813313" POSITION="right" TEXT="Improve API for parameterized coercions"/>
 <node CREATED="1367787818774" ID="ID_1229200854" MODIFIED="1367787837525" POSITION="right" TEXT="Consider ways to reduce duplication when definition a type constraint coderef plus inline code"/>
@@ -11,12 +15,11 @@
 <node CREATED="1365786461715" ID="ID_1123916121" MODIFIED="1365786480681" TEXT="Type-Tiny could be used as a backend for Scalar::Does?">
 <node CREATED="1365786482499" ID="ID_957202187" MODIFIED="1365786492545" TEXT="if not, certainly a SEE ALSO"/>
 </node>
-<node CREATED="1367787879025" ID="ID_940529361" MODIFIED="1367787890608" TEXT="Release stable version of Types-XSD"/>
-<node CREATED="1367787892513" ID="ID_1272020953" MODIFIED="1367787896912" TEXT="Release Type-Tie"/>
 </node>
 <node CREATED="1367787995229" ID="ID_180939573" MODIFIED="1367788009250" POSITION="left" TEXT="Maybe[`a] should have a coercion if `a does???"/>
 <node CREATED="1367788011977" ID="ID_1856879176" MODIFIED="1367788015978" POSITION="left" TEXT="Documentation">
 <node CREATED="1367788018564" ID="ID_716920082" MODIFIED="1367788041234" TEXT="See what questions users come up with, and write more documentation if necessary"/>
 </node>
+<node CREATED="1368653462599" ID="ID_1417938282" MODIFIED="1368653501960" POSITION="right" TEXT="Die if people try to set parent type constraints for TT:Class, TT:Role, etc"/>
 </node>
 </map>
