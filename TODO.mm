5083010ff02acc4b862374f1162f30795a52b57b
Toby Inkster <mail@tobyinkster.co.uk>
2013-06-01 15:27:52 +0100

    todo

--- TODO.mm
+++ TODO.mm
@@ -1,16 +1,23 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1365159230192" ID="ID_28734446" MODIFIED="1367787864051" TEXT="Type-Tiny">
+<node CREATED="1365159230192" ID="ID_28734446" MODIFIED="1370096685063" TEXT="Type-Tiny">
 <node CREATED="1367787794514" ID="ID_1248980001" MODIFIED="1367787813313" POSITION="right" TEXT="Improve API for parameterized coercions"/>
 <node CREATED="1367787818774" ID="ID_1229200854" MODIFIED="1367787837525" POSITION="right" TEXT="Consider ways to reduce duplication when definition a type constraint coderef plus inline code"/>
-<node CREATED="1367787866446" ID="ID_71085740" MODIFIED="1367787870584" POSITION="right" TEXT="Related Projects">
-<node CREATED="1365786461715" ID="ID_1123916121" MODIFIED="1365786480681" TEXT="Type-Tiny could be used as a backend for Scalar::Does?">
-<node CREATED="1365786482499" ID="ID_957202187" MODIFIED="1365786492545" TEXT="if not, certainly a SEE ALSO"/>
-</node>
-</node>
 <node CREATED="1367787995229" ID="ID_180939573" MODIFIED="1367788009250" POSITION="left" TEXT="Maybe[`a] should have a coercion if `a does???"/>
-<node CREATED="1367788011977" ID="ID_1856879176" MODIFIED="1367788015978" POSITION="left" TEXT="Documentation">
+<node CREATED="1367788011977" ID="ID_1856879176" MODIFIED="1370096438248" POSITION="left" TEXT="Documentation">
 <node CREATED="1367788018564" ID="ID_716920082" MODIFIED="1367788041234" TEXT="See what questions users come up with, and write more documentation if necessary"/>
+<node CREATED="1370096440797" ID="ID_1410085323" MODIFIED="1370096448687" TEXT="Document usage with Scalar::Does"/>
+<node CREATED="1370096465025" ID="ID_1832897513" MODIFIED="1370096473773" TEXT="Document usage with Function::Parameters"/>
+</node>
+<node CREATED="1370096488520" ID="ID_1984680804" MODIFIED="1370096493794" POSITION="right" TEXT="Test cases">
+<node CREATED="1370096494999" ID="ID_557686619" MODIFIED="1370096504892" TEXT="Type::Registry needs a few more test cases"/>
+<node CREATED="1370096506648" ID="ID_905303937" MODIFIED="1370096520912" TEXT="Type::Parser needs lots of test cases">
+<node CREATED="1370096525033" ID="ID_1258560264" MODIFIED="1370096558143" TEXT="Check every type of token in the grammar, inclusing qr{}, qq{}, q{}, fat comma, class types, etc"/>
+<node CREATED="1370096559888" ID="ID_740384364" MODIFIED="1370096605958" TEXT="Test precedence works - e.g. Foo|~Bar&amp;Baz"/>
+</node>
+</node>
+<node CREATED="1370096643105" ID="ID_1810538719" MODIFIED="1370096660882" POSITION="left" TEXT="Hassle mst about exception API for constraints and coercions in Moo">
+<node CREATED="1370096662733" ID="ID_407676023" MODIFIED="1370096674044" TEXT="Implement what needs to be implemented at Type::Exception&apos;s end"/>
 </node>
 </node>
 </map>
