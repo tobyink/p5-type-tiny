4b2261297b24e4360c260f51de9b1d1f9a38f102
Toby Inkster <mail@tobyinkster.co.uk>
2013-06-06 20:20:54 +0100

    todo

--- TODO.mm
+++ TODO.mm
@@ -4,9 +4,11 @@
 <node CREATED="1367787794514" ID="ID_1248980001" MODIFIED="1367787813313" POSITION="right" TEXT="Improve API for parameterized coercions"/>
 <node CREATED="1367787818774" ID="ID_1229200854" MODIFIED="1367787837525" POSITION="right" TEXT="Consider ways to reduce duplication when definition a type constraint coderef plus inline code"/>
 <node CREATED="1367787995229" ID="ID_180939573" MODIFIED="1367788009250" POSITION="left" TEXT="Maybe[`a] should have a coercion if `a does???"/>
-<node CREATED="1367788011977" ID="ID_1856879176" MODIFIED="1370096438248" POSITION="left" TEXT="Documentation">
+<node CREATED="1367788011977" ID="ID_1856879176" MODIFIED="1370546308607" POSITION="left" TEXT="Documentation">
 <node CREATED="1367788018564" ID="ID_716920082" MODIFIED="1367788041234" TEXT="See what questions users come up with, and write more documentation if necessary"/>
 <node CREATED="1370096440797" ID="ID_1410085323" MODIFIED="1370096448687" TEXT="Document usage with Scalar::Does"/>
+<node CREATED="1370546314567" ID="ID_212110988" MODIFIED="1370546325695" TEXT="Document Type::Utils - don&apos;t just point people at Moose"/>
+<node CREATED="1370546331881" ID="ID_1551591465" MODIFIED="1370546359337" TEXT="Document Types::Standard - don&apos;t just point people at Moose, MooseX::Types::Structured and MooX::Types::MooseLike::Base"/>
 </node>
 <node CREATED="1370096488520" ID="ID_1984680804" MODIFIED="1370096493794" POSITION="right" TEXT="Test cases">
 <node CREATED="1370096494999" ID="ID_557686619" MODIFIED="1370096504892" TEXT="Type::Registry needs a few more test cases"/>
@@ -19,5 +21,6 @@
 <node CREATED="1370096643105" ID="ID_1810538719" MODIFIED="1370096660882" POSITION="left" TEXT="Hassle mst about exception API for constraints and coercions in Moo">
 <node CREATED="1370096662733" ID="ID_407676023" MODIFIED="1370096674044" TEXT="Implement what needs to be implemented at Type::Exception&apos;s end"/>
 </node>
+<node CREATED="1370546406893" ID="ID_1652672549" MODIFIED="1370546420935" POSITION="right" TEXT="Implement match_on_type from Moose::Util::TypeConstraints"/>
 </node>
 </map>
