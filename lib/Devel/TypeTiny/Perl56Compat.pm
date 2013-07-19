0b7d997ccef5aeb93ca848353751e86708626fba
Toby Inkster <mail@tobyinkster.co.uk>
2013-07-19 11:37:13 +0100

    documentation for Devel::TypeTiny::Perl56Compat

--- lib/Devel/TypeTiny/Perl56Compat.pm
+++ lib/Devel/TypeTiny/Perl56Compat.pm
@@ -1,11 +1,57 @@
+package Devel::TypeTiny::Perl56Compat;
+our $AUTHORITY = 'cpan:TOBYINK';
+our $VERSION   = '0.017_01';
+
 #### B doesn't provide perlstring() in 5.6. Monkey patch it.
 
 use B ();
-
 *B::perlstring = sub {
 	sprintf('"%s"', quotemeta($_[0]))
 } unless exists &B::perlstring;
-
 push @B::EXPORT_OK, 'perlstring';
 
-1;
+#### Done!
+
+5.6;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=for stopwords pragmas
+
+=head1 NAME
+
+Devel::TypeTiny::Perl56Compat - shims to allow Type::Tiny to run on Perl 5.6.x
+
+=head1 DESCRIPTION
+
+This is not considered part of Type::Tiny's public API.
+
+Currently this module just has one job: it patches L<B> to export a
+C<perlstring> function, as this was only added in Perl 5.8.0.
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Type-Tiny>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2013 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
