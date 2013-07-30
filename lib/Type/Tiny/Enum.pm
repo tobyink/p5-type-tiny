9a8b5871c96d67f611ecc9542d5264ae8aa2159f
Toby Inkster <mail@tobyinkster.co.uk>
2013-07-30 10:30:10 +0100

    fix Enum regexps

--- lib/Type/Tiny/Enum.pm
+++ lib/Type/Tiny/Enum.pm
@@ -46,7 +46,7 @@ sub _build_constraint
 {
 	my $self = shift;
 	my $regexp = join "|", map quotemeta, @$self;
-	return sub { defined and m{^(?:$regexp)$} };
+	return sub { defined and m{\A(?:$regexp)\z} };
 }
 
 sub can_be_inlined
@@ -59,8 +59,8 @@ sub inline_check
 	my $self = shift;
 	my $regexp = join "|", map quotemeta, @$self;
 	$_[0] eq '$_'
-		? "(defined and m{^(?:$regexp)\$})"
-		: "(defined($_[0]) and $_[0] =~ m{^(?:$regexp)\$})";
+		? "(defined and m{\\A(?:$regexp)\\z})"
+		: "(defined($_[0]) and $_[0] =~ m{\\A(?:$regexp)\\z})";
 }
 
 sub _instantiate_moose_type
