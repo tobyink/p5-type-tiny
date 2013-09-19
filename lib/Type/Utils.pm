dd81fe4ddd40c27ae5ffe053d94c18f77c9611eb
Toby Inkster <mail@tobyinkster.co.uk>
2013-09-19 09:41:10 +0100

    fix documentation typo; RT#88798

--- lib/Type/Utils.pm
+++ lib/Type/Utils.pm
@@ -592,7 +592,7 @@ types declared in the package will be automatically installed into the
 library.
 
 Hidden gem: if you're inheriting from a type constraint that includes some
-coercions, you can include C<< coerce => 1 >> in the C<< %options >> hash
+coercions, you can include C<< coercion => 1 >> in the C<< %options >> hash
 to inherit the coercions.
 
 =item C<< subtype $name, %options >>
