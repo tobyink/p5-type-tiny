256ac4dbd5ccf9a10924dcfc34be45f3d7422269
Toby Inkster <mail@tobyinkster.co.uk>
2014-01-20 22:46:27 +0000

    script for figuring out what modules need test cases

---
+++ t/not-covered.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+
+use v5.014;
+use Path::Tiny;
+use Path::Iterator::Rule;
+
+use constant LIB_DIR  => path(path(__FILE__)->absolute->dirname)->parent->child('lib');
+use constant TEST_DIR => path(path(__FILE__)->absolute->dirname)->parent->child('t/modules');
+
+my $rule = Path::Iterator::Rule->new->file->perl_module;
+my $iter = $rule->iter( LIB_DIR );
+
+while (my $file = $iter->())
+{
+	my $module = path($file)->relative(LIB_DIR);
+	$module =~ s{.pm$}{};
+	$module =~ s{/}{::}g;
+	
+	TEST_DIR->child($module =~ s/::/-/gr)->exists
+		or say $module;
+}
