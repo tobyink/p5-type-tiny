1c2823e2f1cb428eabdcb6106433f557ef665c4b
Toby Inkster <mail@tobyinkster.co.uk>
2014-02-05 17:16:19 +0000

    oops

--- lib/Type/Tiny/_HalfOp.pm
+++ lib/Type/Tiny/_HalfOp.pm
@@ -1,4 +1,4 @@
-package Type::Tiny::HalfOp;
+package Type::Tiny::_HalfOp;
 
 use 5.006001;
 use strict;
@@ -23,3 +23,66 @@ sub complete {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=for stopwords pragmas
+
+=head1 NAME
+
+Type::Tiny::_HalfOp - half-completed overloaded operation
+
+=head1 STATUS
+
+This module is considered part of Type-Tiny's internals. It is not
+covered by the
+L<Type-Tiny stability policy|Type::Tiny::Manual::Policies/"STABILITY">.
+
+=head1 DESCRIPTION
+
+This is not considered part of Type::Tiny's public API.
+
+It is a class representing a half-completed overloaded operation.
+
+=head2 Constructor
+
+=over
+
+=item C<< new($operation, $param, $type) >>
+
+=back
+
+=head2 Method
+
+=over
+
+=item C<< complete($type) >>
+
+=back
+
+=head1 BUGS
+
+Please report any bugs to
+L<http://rt.cpan.org/Dist/Display.html?Queue=Type-Tiny>.
+
+=head1 AUTHOR
+
+Graham Knopp E<lt>haarg@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2014 by Graham Knopp.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
--- t/20-unit/Type-Tiny-_HalfOp/overload-precedence.t
+++ t/20-unit/Type-Tiny-_HalfOp/overload-precedence.t
@@ -1,3 +1,26 @@
+=pod
+
+=encoding utf-8
+
+=head1 PURPOSE
+
+Ensure that the following works consistently on all supported Perls:
+
+   ArrayRef[Int] | HashRef[Int]
+
+=head1 AUTHOR
+
+Graham Knopp E<lt>haarg@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2014 by Graham Knopp.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
 use strict;
 use warnings FATAL => 'all';
 use Test::More;
@@ -7,13 +30,13 @@ use Types::Standard -all;
 my $union = eval { ArrayRef[Int] | HashRef[Int] };
 
 SKIP: {
-  ok $union or skip 'broken type', 6;
-  ok $union->check({welp => 1});
-  ok !$union->check({welp => 1.4});
-  ok !$union->check({welp => "guff"});
-  ok $union->check([1]);
-  ok !$union->check([1.4]);
-  ok !$union->check(["guff"]);
+	ok $union or skip 'broken type', 6;
+	ok $union->check({welp => 1});
+	ok !$union->check({welp => 1.4});
+	ok !$union->check({welp => "guff"});
+	ok $union->check([1]);
+	ok !$union->check([1.4]);
+	ok !$union->check(["guff"]);
 }
 
 done_testing;
