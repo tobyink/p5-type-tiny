ff9f98bef1f8f5bb93e1a59854b0910d3ffd3589
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-07 22:01:28 +0100

    want XS 0.011

--- lib/Type/Tiny.pm
+++ lib/Type/Tiny.pm
@@ -11,7 +11,7 @@ BEGIN {
 BEGIN {
 	$Type::Tiny::AUTHORITY   = 'cpan:TOBYINK';
 	$Type::Tiny::VERSION     = '1.001_000';
-	$Type::Tiny::XS_VERSION  = '0.010';
+	$Type::Tiny::XS_VERSION  = '0.011';
 }
 
 use Eval::TypeTiny ();
