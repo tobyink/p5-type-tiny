4b8216465c25894d4c9f9ace16718e345d8b7bdd
Toby Inkster <mail@tobyinkster.co.uk>
2018-04-17 17:50:04 +0100

    RT#123408

--- lib/Types/Standard.pm
+++ lib/Types/Standard.pm
@@ -33,7 +33,10 @@ BEGIN {
 			return !!1 if exists $stash->{'ISA'};
 			return !!1 if exists $stash->{'VERSION'};
 			foreach my $globref (values %$stash) {
-				return !!1 if *{$globref}{CODE};
+				return !!1
+				  if ref \$globref eq 'GLOB'
+				     ? *{$globref}{CODE}
+				     : ref $globref; # const or sub ref
 			}
 			return !!0;
 		};
