3726df5a8d1299f624270f42ed3e059f10ce4ffd
Toby Inkster <mail@tobyinkster.co.uk>
2019-01-08 23:29:45 +0000

    make Eval::TypeTiny API stable; list a few other internal modules

--- lib/Eval/TypeTiny.pm
+++ lib/Eval/TypeTiny.pm
@@ -299,9 +299,8 @@ __END__
 Eval::TypeTiny - utility to evaluate a string of Perl code in a clean environment
 
 =head1 STATUS
-
-This module is considered part of Type-Tiny's internals. It is not
-covered by the
+ 
+This module is covered by the
 L<Type-Tiny stability policy|Type::Tiny::Manual::Policies/"STABILITY">.
 
 =head1 DESCRIPTION
