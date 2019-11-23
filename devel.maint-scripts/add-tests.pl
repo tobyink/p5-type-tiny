f543524128528f3c467fba970026cff7766c7707
Toby Inkster <mail@tobyinkster.co.uk>
2019-11-23 14:56:34 +0000

    my script for automatically adding new tests

---
+++ devel.maint-scripts/add-tests.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Path::Tiny qw( path );
+
+#	bleh => 'regexp'                   => qr/./,
+my $new = <<'NEW_LINES';
+	fail => 'object stringifying to ""' => do { package Local::OL::StringEmpty; use overload q[""] => sub { "" }; bless [] },
+	fail => 'object stringifying to "1"' => do { package Local::OL::StringOne; use overload q[""] => sub { "1" }; bless [] },
+	fail => 'object numifying to 0'    => do { package Local::OL::NumZero; use overload q[0+] => sub { 0 }; bless [] },
+	fail => 'object numifying to 1'    => do { package Local::OL::NumOne; use overload q[0+] => sub { 1 }; bless [] },
+	fail => 'object overloading arrayref' => do { package Local::OL::Array; use overload q[@{}] => sub { $_[0]{array} }; bless {array=>[]} },
+	fail => 'object overloading hashref' => do { package Local::OL::Hash; use overload q[%{}] => sub { $_[0][0] }; bless [{}] },
+	fail => 'object overloading coderef' => do { package Local::OL::Code; use overload q[&{}] => sub { $_[0][0] }; bless [sub { 1 }] },
+NEW_LINES
+
+my $dir = path('t/21-types/');
+for my $file ($dir->children) {
+	my @lines = map {
+		$_ =~ /^#TESTS/ ? ($new, $_) : $_
+	} $file->lines_utf8;
+	$file->spew_utf8(@lines);
+}
