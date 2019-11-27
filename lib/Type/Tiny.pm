c151a9a0f99c86f38e0493fefd008a2016b2aa51
Toby Inkster <mail@tobyinkster.co.uk>
2019-11-27 17:37:40 +0000

    I am rarely on IRC, so stop recommending #moops for support

--- lib/Type/Tiny.pm
+++ lib/Type/Tiny.pm
@@ -2147,11 +2147,6 @@ of parts of Type::Tiny.
 Please report any bugs to
 L<http://rt.cpan.org/Dist/Display.html?Queue=Type-Tiny>.
 
-=head1 SUPPORT
-
-B<< IRC: >> support is available through in the I<< #moops >> channel
-on L<irc.perl.org|http://www.irc.perl.org/channels.html>.
-
 =head1 SEE ALSO
 
 L<Type::Tiny::Manual>, L<Type::API>.
