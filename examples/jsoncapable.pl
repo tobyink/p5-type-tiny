46412eeca19304c43727d422d1c85b05108745e9
Toby Inkster <mail@tobyinkster.co.uk>
2020-10-01 14:24:38 +0100

    JSONCapable example

---
+++ examples/jsoncapable.pl
@@ -0,0 +1,29 @@
+use strict;
+use warnings;
+use feature 'say';
+
+BEGIN {
+	package My::Types;
+	use Type::Library 1.011005
+		-utils,
+		-extends => [ 'Types::Standard' ],
+		-declare => 'JSONCapable';
+	
+	declare JSONCapable,
+		as Undef
+		|  ScalarRef[ Enum[ 0..1 ] ]
+		|  Num
+		|  Str
+		|  ArrayRef[ JSONCapable ]
+		|  HashRef[ JSONCapable ]
+		;
+}
+
+use My::Types 'is_JSONCapable';
+
+my $var = {
+	foo => 1,
+	bar => [ \0, "baz", [] ],
+};
+
+say is_JSONCapable $var;
