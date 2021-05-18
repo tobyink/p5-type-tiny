ca263b1ee3cdadefae15abb829d245a617e74894
Lucas Tiago de Moraes <lucastiagodemoraes@gmail.com>
2021-05-18 00:27:58 -0300

    Solved example that returned warning
    Code:
    https://pastebin.com/b3hc21S6
    
    Warning:
    Use of uninitialized value $_ in pattern match (m//) at test.pl line 12.

--- lib/Types/Standard.pm
+++ lib/Types/Standard.pm
@@ -1511,7 +1511,7 @@ Here's an example using L<Regexp::Common>:
       has ip_address => (
          is         => 'ro',
          required   => 1,
-         isa        => StrMatch[/^$RE{net}{IPv4}$/],
+         isa        => StrMatch[qr/^$RE{net}{IPv4}$/],
          default    => '127.0.0.1',
       );
    }
