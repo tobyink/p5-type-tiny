2cac651e6030ff6819daaf55eb08e2d4aa13481b
Toby Inkster <mail@tobyinkster.co.uk>
2022-07-20 17:20:23 +0100

    Documentation and tests for Eval::TypeTiny::CodeAccumulator

--- lib/Eval/TypeTiny/CodeAccumulator.pm
+++ lib/Eval/TypeTiny/CodeAccumulator.pm
@@ -23,6 +23,7 @@ sub new {
 	$self{env}          ||= {};
 	$self{code}         ||= [];
 	$self{placeholders} ||= {};
+	$self{indent}       ||= '';
 
 	bless \%self, $class;
 }
@@ -33,11 +34,21 @@ sub env         { $_[0]{env} }
 
 sub add_line {
 	my $self = shift;
-	my $indent = $self->{indent} || '';
+	my $indent = $self->{indent};
 
 	push @{ $self->{code} }, map { $indent . $_ } map { split /\n/ } @_;
 
 	$self;
+}
+
+sub increase_indent {
+	$_[0]{indent} .= "\t";
+	$_[0];
+}
+
+sub decrease_indent {
+	$_[0]{indent} =~ s/\t$//;
+	$_[0];
 }
 
 sub add_gap {
@@ -93,3 +104,142 @@ sub compile {
 }
 
 1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=for stopwords pragmas coderefs
+
+=head1 NAME
+
+Eval::TypeTiny::CodeAccumulator - alternative API for Eval::TypeTiny
+
+=head1 SYNOPSIS
+
+  my $make_adder = 'Eval::TypeTiny::CodeAccumulator'->new(
+    description => 'adder',
+  );
+  
+  my $n = 40;
+  my $varname = $make_adder->add_variable( '$addend' => \$n );
+  
+  $make_adder->add_line( 'sub {' );
+  $make_adder->increase_indent;
+  $make_adder->add_line( 'my $other_addend = shift;' );
+  $make_adder->add_gap;
+  $make_adder->add_line( 'return ' . $varname . ' + $other_addend;' );
+  $make_adder->decrease_indent;
+  $make_adder->add_line( '}' );
+  
+  my $adder = $make_adder->compile;
+  
+  say $adder->( 2 );  ## ==> 42
+
+=head1 STATUS
+ 
+This module is covered by the
+L<Type-Tiny stability policy|Type::Tiny::Manual::Policies/"STABILITY">.
+
+=head1 DESCRIPTION
+
+=head2 Constructor
+
+=over
+
+=item C<< new( %attrs ) >>
+
+The only currently supported attribute is C<description>.
+
+=back
+
+=head2 Methods
+
+=over
+
+=item C<< env() >>
+
+Returns the current compilation environment, a hashref of variables to close
+over.
+
+=item C<< code() >>
+
+Returns the source code so far.
+
+=item C<< description() >>
+
+Returns the same description given to the constructor, if any.
+
+=item C<< add_line( $line_of_code ) >>
+
+Adds the next line of code.
+
+=item C<< add_gap() >>
+
+Adds a blank line of code.
+
+=item C<< increase_indent() >>
+
+Increases the indentation level for subsequent lines of code.
+
+=item C<< decrease_indent() >>
+
+Decreases the indentation level for subsequent lines of code.
+
+=item C<< add_variable( $varname, $reference_to_value ) >>
+
+Adds a variable to the compilation environment so that the coderef being
+generated can close over it.
+
+If a variable already exists in the environment with that name, will instead
+add a variable with a different name and return that name. You should always
+continue to refer to the variable with that returned name, just in case.
+
+=item C<< add_placeholder( $placeholder_name ) >>
+
+Adds a line of code which is just a comment, but remembers its line number.
+
+=item C<< fill_placeholder( $placeholder_name, $line_of_code ) >>
+
+Goes back to a previously inserted placeholder and replaces it with code.
+
+Currently doesn't play nice with indentation.
+
+=item C<< compile() >>
+
+Compiles the code and returns it as a coderef.
+
+=item C<< finalize() >>
+
+This method does nothing but is called by C<compile> just before compiling
+the code. It may be useful if subclassing this class.
+
+=back
+
+=head1 BUGS
+
+Please report any bugs to
+L<https://github.com/tobyink/p5-type-tiny/issues>.
+
+=head1 SEE ALSO
+
+L<Eval::TypeTiny>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2022 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
---
+++ t/20-modules/Eval-TypeTiny-CodeAccumulator/basic.t
@@ -0,0 +1,52 @@
+=pod
+
+=encoding utf-8
+
+=head1 PURPOSE
+
+Tests L<Eval::TypeTiny::CodeAccumulator>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2022 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+use strict;
+use warnings;
+use Test::More;
+
+ok( require Eval::TypeTiny::CodeAccumulator );
+
+my $make_adder = 'Eval::TypeTiny::CodeAccumulator'->new(
+	description => 'adder',
+);
+
+my $n = 40;
+my $varname = $make_adder->add_variable( '$addend' => \$n );
+
+is $varname, '$addend';
+is $make_adder->add_variable( '$addend' => \999 ), '$addend_1';
+
+$make_adder->add_line( 'sub {' );
+$make_adder->increase_indent;
+$make_adder->add_line( 'my $other_addend = shift;' );
+$make_adder->add_gap;
+$make_adder->add_line( 'return ' . $varname . ' + $other_addend;' );
+$make_adder->decrease_indent;
+$make_adder->add_line( '}' );
+
+note( $make_adder->code );
+
+my $adder = $make_adder->compile;
+
+is $adder->( 2 ), 42;
+
+done_testing;
