aaa767e09f8c9448fa3410f2a70f900fcdf61b31
Toby Inkster <mail@tobyinkster.co.uk>
2022-09-20 15:52:20 +0100

    Remove pod from some internal modules

--- lib/Types/Standard/HashRef.pm
+++ lib/Types/Standard/HashRef.pm
@@ -1,3 +1,5 @@
+# INTERNAL MODULE: guts for HashRef type from Types::Standard.
+
 package Types::Standard::HashRef;
 
 use 5.008001;
@@ -163,50 +165,3 @@ sub __hashref_allows_value {
 } #/ sub __hashref_allows_value
 
 1;
-
-__END__
-
-=pod
-
-=encoding utf-8
-
-=head1 NAME
-
-Types::Standard::HashRef - internals for the Types::Standard HashRef type constraint
-
-=head1 STATUS
-
-This module is considered part of Type-Tiny's internals. It is not
-covered by the
-L<Type-Tiny stability policy|Type::Tiny::Manual::Policies/"STABILITY">.
-
-=head1 DESCRIPTION
-
-This file contains some of the guts for L<Types::Standard>.
-It will be loaded on demand. You may ignore its presence.
-
-=head1 BUGS
-
-Please report any bugs to
-L<https://github.com/tobyink/p5-type-tiny/issues>.
-
-=head1 SEE ALSO
-
-L<Types::Standard>.
-
-=head1 AUTHOR
-
-Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
-
-=head1 COPYRIGHT AND LICENCE
-
-This software is copyright (c) 2013-2014, 2017-2022 by Toby Inkster.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=head1 DISCLAIMER OF WARRANTIES
-
-THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
-WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
-MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
